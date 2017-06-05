88d72ca276071b4558f67da40ac39ecf4888012d
karchevz <zheko.karchev@gmail.com>
2017-06-04 22:37:21 -0400

    Added nav bar and added default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist 
   include SetSource
-
-  def current_user
-    super || guest_user
-  end
-
-  def guest_user
-    OpenStruct.new(name: "Guest User", 
-                  first_name: "Guest", 
-                  last_name: "User", 
-                  email: "guest@example.com"
-                  )
-  end
+  include CurrentUserConcern
+  include DefaultPageContent   
 
   #below is actions for adding params search query
   #before_action :set_source
